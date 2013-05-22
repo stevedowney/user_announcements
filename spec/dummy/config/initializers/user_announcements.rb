2ff75c365a31be9b11bdf4451a9abce96781a2a6
Steve Downey <steve.downtown@gmail.com>
2013-05-22 13:45:36 -0700

    update ignore

--- spec/dummy/config/initializers/user_announcements.rb
+++ spec/dummy/config/initializers/user_announcements.rb
@@ -1,6 +1,7 @@
 UserAnnouncements.config do |c|
   
-  # uncomment to user Bootstrap styling
-  # c.bootstrap = true
+  # control Bootstrap styling
+  # c.bootstrap = false
+  c.bootstrap = true
   
 end
